3cb145e89914b276822068a36f3f51ae98d29e0d
Prince <hakwumerunyeche@gmail.com>
2025-10-06 23:21:25 +0100

    Step 4: Add test suites
    - Add test suites
    - Cypress test
    - Playwright test
    - Both can be used interchangeably

---
+++ webpack.config.ts
@@ -0,0 +1,17 @@
+// @ts-expect-error path detected error
+import path from 'path';
+// @ts-expect-error webpack detected error
+import webpack from 'webpack';
+// in case you run into any typescript error when configuring `devServer`
+import 'webpack-dev-server';
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './foo.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'foo.bundle.js',
+  },
+};
+
+export default config;
